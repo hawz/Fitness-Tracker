ed0c64b4fc00df0011439d7bebcf15916c57ea1e
Alberto Masia <alberto.masia@gmail.com>
2019-02-16 16:45:09 +0100

    Use isAuth in Header component.

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth: boolean;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.subscription = this.authService.authChange.subscribe(
+      (authStatus: boolean) => {
+        this.isAuth = authStatus;
+      }
+    );
   }
 
   onToggleSidenav() {
     this.sidenavToggle.emit();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  // the subscription should unsubscribe whenever the component is destroyed
+  // so that's why we need the ngOnDestroy lifecycle hook
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
