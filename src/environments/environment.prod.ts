c1890d8d305b4ffa5c42bdb0c557ffc7051e184e
Alberto Masia <alberto.masia@gmail.com>
2019-02-18 16:57:58 +0100

    Add firebase configuration to environment.prod.ts file.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBmtwTL8UuLot3T8-Nh7Q0G4-LrVgB5hpk',
+    authDomain: 'ng-fitness-tracker-acfe7.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-acfe7.firebaseio.com',
+    projectId: 'ng-fitness-tracker-acfe7',
+    storageBucket: 'ng-fitness-tracker-acfe7.appspot.com',
+    messagingSenderId: '593636989843'
+  }
 };
