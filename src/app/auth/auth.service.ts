dbb4efc050ad3502600fa95e60b85a67c0c2ad8c
Alberto Masia <alberto.masia@gmail.com>
2019-03-06 18:04:31 +0100

    Create auth reducer and auth actions and use them inside auth service.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,24 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Store } from '@ngrx/store';
 
-import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 import { TrainingService } from '../training/training.service';
 import { UIService } from '../shared/ui.service';
 import * as fromRoot from '../app.reducer';
 import * as UI from '../shared/ui.actions';
+import * as Auth from '../auth/auth.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  authChange = new Subject<boolean>();
-  private isAuthenticated = false;
-  private user: User;
-
   constructor(
     private router: Router,
     private afAuth: AngularFireAuth,
@@ -30,23 +25,31 @@ export class AuthService {
   initAuthListener() {
     this.afAuth.authState.subscribe(user => {
       if (user) {
-        this.isAuthenticated = true;
-        this.authChange.next(true);
+        // this.isAuthenticated = true;
+        // this.authChange.next(true);
+        this.store.dispatch(new Auth.SetAuthenticated());
         this.router.navigate(['/training']);
       } else {
         this.trainingService.cancelSubscriptions();
-        this.isAuthenticated = false;
-        this.authChange.next(false);
+        // this.isAuthenticated = false;
+        // this.authChange.next(false);
+        this.store.dispatch(new Auth.SetUnauthenticated());
         this.router.navigate(['/login']);
       }
     });
   }
 
   registerUser(authData: AuthData) {
+    // here it was
+    // this.uiService.loadingStateChanged.next(true);
+    // now with NgRx it becomes
     this.store.dispatch(new UI.StartLoading());
     this.afAuth.auth
       .createUserWithEmailAndPassword(authData.email, authData.password)
       .then(result => {
+        // here it was
+        // this.uiService.loadingStateChanged.next(false);
+        // now with NgRx it becomes
         this.store.dispatch(new UI.StopLoading());
       })
       .catch(error => {
@@ -70,9 +73,5 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();
-  }
-
-  isAuth() {
-    return this.isAuthenticated;
   }
 }
