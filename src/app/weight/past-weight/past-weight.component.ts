0cc5f23e5839a46f0a978d86da8f3c3538739b24
Alberto Masia <alberto.masia@gmail.com>
2019-03-06 15:04:54 +0100

    Add paginator and sorting for table in past weight section.

--- src/app/weight/past-weight/past-weight.component.ts
+++ src/app/weight/past-weight/past-weight.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { WeightService } from '../weight.service';
 import { Weight } from '../weight.model';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-past-weight',
   templateUrl: './past-weight.component.html',
   styleUrls: ['./past-weight.component.css']
 })
-export class PastWeightComponent implements OnInit {
+export class PastWeightComponent implements OnInit, AfterViewInit, OnDestroy {
   displayedColumns = ['date', 'weight'];
   dataSource = new MatTableDataSource<Weight>();
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   private weightChangedSubscription: Subscription;
 
   constructor(private weightService: WeightService) { }
@@ -22,8 +26,18 @@ export class PastWeightComponent implements OnInit {
         this.dataSource.data = weights;
       }
     );
-
     this.weightService.fetchWeightMeasurements();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnDestroy() {
+    if (this.weightChangedSubscription) {
+      this.weightChangedSubscription.unsubscribe();
+    }
   }
 
 }
